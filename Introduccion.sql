c0042ef029d0ab1c72138fa600974600a7a70a7e
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-06-19 14:58:32 -0500

    feat: :card_file_box: Introduccion Manejo `Base de datos`.
    Introduccion.sql

---
+++ Introduccion.sql
@@ -0,0 +1,9 @@
+-- Bases de Datos
+
+CREATE DATABASE mi_base_de_datos; -- Crear base de datos
+
+SHOW DATABASES; -- Mostrar Bases de datos
+
+USE mi_base_de_datos; -- Usar base de datos
+
+DROP DATABASE IF EXISTS mi_base_de_datos;
